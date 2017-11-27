b43c77513ae00e423eb6fa54b187a102f6726b9e
Jenky Nolasco <jenky.nolasco@gmail.com>
2017-11-27 18:00:39 -0500

    Logged body request

--- src/api/webhook/index.js
+++ src/api/webhook/index.js
@@ -8,6 +8,7 @@ const WEBHOOK_URL = 'https://hooks.zapier.com/hooks/catch/1160582/szth43/'
 webhook.post('/', async ctx => {
   const { body } = ctx.request
   try {
+    console.log(body)
     const res = await axios.post(WEBHOOK_URL, body)
     if(res.statusText === 'OK') return ctx.body = { ok : true, message : 'Data sent ok' }
     return ctx.body = { ok : false, message : 'Status Text not OK'}

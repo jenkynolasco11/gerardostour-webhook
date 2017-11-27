cd4931f443de528d073011aefd9fee295b76456f
Jenky Nolasco <jenky.nolasco@gmail.com>
2017-11-27 17:33:44 -0500

    Added post forwarder

--- src/api/webhook/index.js
+++ src/api/webhook/index.js
@@ -1,9 +1,14 @@
 import Router from 'koa-router'
+import axios from 'axios'
 
 const webhook = new Router({ prefix : 'webhook'})
 
-webhook.post('/', ctx => {
+const WEBHOOK_URL = 'https://hooks.zapier.com/hooks/catch/1160582/szth43/'
+
+webhook.post('/', async ctx => {
   const { body } = ctx.request
+
+  const res = await axios.post(WEBHOOK_URL, body)
 })
 
 export default webhook

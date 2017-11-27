55222355bbbd19ee770c2a10d0e85d99927bd2a5
Jenky Nolasco <jenky.nolasco@gmail.com>
2017-11-27 17:46:12 -0500

    Added extra route

--- src/api/index.js
+++ src/api/index.js
@@ -6,6 +6,10 @@ const api = new Router({ prefix : '/api/v1'})
 
 api.use('/', webhook.routes(), webhook.allowedMethods())
 
+api.get('/', ctx => {
+  return ctx.body = { ok : true, message : 'it works!' }
+})
+
 api.stack.forEach(e => console.log(e.path))
 
 export default api.routes()
